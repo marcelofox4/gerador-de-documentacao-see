cae530f8bc90b129eb81b9f409f198dd5f60945a
marcelofox4 <marcelofox4@hotmail.com>
2024-02-11 20:32:04 -0300

    Configuração para implementação da documentação com Swagger

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import "reflect-metadata"
 import express, { Request, Response, NextFunction } from 'express';
 import  { createConnection }  from "typeorm";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
 createConnection();
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(port, () => console.log(`Server is running in port: ${port}!`));
