c4229dfee4f7f769e4e7cbdd4a75cb93c8dba887
marcelofox4 <marcelofox4@hotmail.com>
2024-02-11 20:37:16 -0300

    Configuração e implementação dos tratamentos de erros na aplicação

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import  { createConnection }  from "typeorm";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 
+import { AppError } from "./shared/errors/AppError";
+import "express-async-errors";
+
 createConnection();
 
 const app = express();
@@ -12,5 +15,17 @@ const port = 3333;
 
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    });
+});
 
 app.listen(port, () => console.log(`Server is running in port: ${port}!`));
