63aeacb7fe4cc41b388e2f5ec01bf95e15292ac5
marcelofox4 <marcelofox4@hotmail.com>
2024-02-17 22:47:44 -0300

    Implementação do caso de uso Find Address By ID  (controller - route - documentation (Swagger)).

--- src/shared/routes/address.routes.ts
+++ src/shared/routes/address.routes.ts
@@ -4,17 +4,21 @@ import { CreateAddressController } from "../../modules/address/useCases/createAd
 import { ListAddressesController } from "../../modules/address/useCases/listAddresses/ListAddressesController";
 import { UpdateAddressController } from "../../modules/address/useCases/updateAddress/UpdateAddressController";
 import { DeleteAddressController } from "../../modules/address/useCases/deleteAddress/DeleteAddressController";
+import { FindAddressByIdController } from "../../modules/address/useCases/findAddressById/FindAddressByIdController";
 
 const addressRoutes = Router();
 
 let createAddressController = new CreateAddressController();
 let listAddressesController = new ListAddressesController();
+let findAddressByIdController = new FindAddressByIdController();
 let updateAddressController = new UpdateAddressController();
 let deleteAddressController = new DeleteAddressController();
 
 addressRoutes.post("/", createAddressController.handle);
 
 addressRoutes.get("/", listAddressesController.handle);
+
+addressRoutes.get("/:id", findAddressByIdController.handle);
 
 addressRoutes.put("/:id", updateAddressController.handle);
 
