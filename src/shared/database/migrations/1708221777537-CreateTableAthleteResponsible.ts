86114ff7d532f420c55784655e422e8a6d459711
marcelofox4 <marcelofox4@hotmail.com>
2024-02-17 23:56:57 -0300

    Configuração e implementação da migration e da entidade - AthleteResponsible

---
+++ src/shared/database/migrations/1708221777537-CreateTableAthleteResponsible.ts
@@ -0,0 +1,84 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateTableAthleteResponsible1708221777537 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "athleteResponsible",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "cpf",
+                        type: "varchar",
+                    },
+                    {
+                        name: "rg",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isNullable: true
+                    },
+                    {
+                        name: "phoneNumber",
+                        type: "varchar",
+                    },
+                    {
+                        name: "profession",
+                        type: "varchar",
+                    },
+                    {
+                        name: "maritalStatus",
+                        type: "varchar",
+                    },
+                    {
+                        name: "addressId",
+                        type: "uuid",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "update_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            "athleteResponsible",
+            new TableForeignKey({
+                name: "FKAddress",
+                referencedTableName: "address",
+                referencedColumnNames: ["id"],
+                columnNames: ["addressId"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL"
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            "athleteResponsible",
+            "FKAddress"
+        );
+
+        await queryRunner.dropTable("athleteResponsible");
+    }
+
+}
