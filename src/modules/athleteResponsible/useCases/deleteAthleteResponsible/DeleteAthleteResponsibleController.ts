63b9c7b408494726bb75627567143d9db6ad1ac5
marcelofox4 <marcelofox4@hotmail.com>
2024-02-19 16:22:36 -0300

    Implementação do caso de uso Delete Athletes Responsible (controller - route - documentation (Swagger)).

---
+++ src/modules/athleteResponsible/useCases/deleteAthleteResponsible/DeleteAthleteResponsibleController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { DeleteAthleteResponsibleUseCase } from "./DeleteAthleteResponsibleUseCase";
+
+class DeleteAthleteResponsibleController {
+
+    async handle(request: Request, response: Response): Promise<Response> {
+
+        const cpf = request.params.cpf;
+
+        const deleteAthleteResponsibleUseCase = container.resolve(DeleteAthleteResponsibleUseCase);
+
+        const athleteResponsibleDeleted = await deleteAthleteResponsibleUseCase.execute(cpf);
+
+        return response.status(200).json(athleteResponsibleDeleted);
+    }
+}
+
+export { DeleteAthleteResponsibleController }
--- src/shared/routes/athleteResponsible.routes.ts
+++ src/shared/routes/athleteResponsible.routes.ts
@@ -3,6 +3,7 @@ import { CreateAthleteResponsibleController } from "../../modules/athleteRespons
 import { ListAthleteResponsibleController } from "../../modules/athleteResponsible/useCases/listAthletesResponsible/ListAthleteResponsibleController";
 import { FindAthleteResponsibleByCpfController } from "../../modules/athleteResponsible/useCases/findAthleteResponsibleByCpf/FindAthleteResponsibleByCpfController";
 import { UpdateAthleteResponsibleController } from "../../modules/athleteResponsible/useCases/updateAthleteResponsible/UpdateAthleteResponsibleController";
+import { DeleteAthleteResponsibleController } from "../../modules/athleteResponsible/useCases/deleteAthleteResponsible/DeleteAthleteResponsibleController";
 
 const athleteResponsibleRoutes = Router();
 
@@ -10,6 +11,7 @@ let createAthleteResponsibleController = new CreateAthleteResponsibleController(
 let listAthleteResponsibleController = new ListAthleteResponsibleController();
 let findAthleteResponsibleByCpfController = new FindAthleteResponsibleByCpfController();
 let updateAthleteResponsibleController = new UpdateAthleteResponsibleController();
+let deleteAthleteResponsibleController = new DeleteAthleteResponsibleController();
 
 athleteResponsibleRoutes.post("/", createAthleteResponsibleController.handle);
 
@@ -18,5 +20,7 @@ athleteResponsibleRoutes.get("/", listAthleteResponsibleController.handle);
 athleteResponsibleRoutes.get("/:cpf", findAthleteResponsibleByCpfController.handle);
 
 athleteResponsibleRoutes.put("/:cpf", updateAthleteResponsibleController.handle);
+
+athleteResponsibleRoutes.delete("/:cpf", deleteAthleteResponsibleController.handle);
 
 export { athleteResponsibleRoutes };
