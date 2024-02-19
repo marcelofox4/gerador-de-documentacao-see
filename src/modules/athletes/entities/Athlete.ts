55c616d06e80286cfdb423fac224ac9bf0854f45
marcelofox4 <marcelofox4@hotmail.com>
2024-02-19 17:30:37 -0300

    Implementação da Entidade (Athlete)

---
+++ src/modules/athletes/entities/Athlete.ts
@@ -0,0 +1,59 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { Address } from "./Address";
+
+@Entity("athlete")
+class AthleteResponsible {
+
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    cpf: string;
+
+    @Column()
+    rg: string;
+
+    @Column({ nullable: true })
+    email?: string;
+
+    @Column({ nullable: true })
+    phoneNumber?: string;
+
+    @Column({ nullable: true })
+    profession?: string;
+
+    @Column({ nullable: true })
+    maritalStatus?: string;
+
+    @Column()
+    addressId?: string;
+
+    @OneToOne(() => Address)
+    @JoinColumn({ name: "addressId" })
+    address: Address;
+
+    @Column()
+    athleteResponsibleId?: string;
+
+    @OneToOne(() => AthleteResponsible)
+    @JoinColumn({ name: "athleteResponsibleId" })
+    athleteResponsible: AthleteResponsible;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @CreateDateColumn()
+    update_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { AthleteResponsible };
