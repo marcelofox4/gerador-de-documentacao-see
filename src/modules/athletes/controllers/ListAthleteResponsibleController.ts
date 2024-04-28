41f1bb5ed9468c2f6aa63bcfcdaa907c20596e90
marcelofox4 <marcelofox4@hotmail.com>
2024-04-28 13:09:18 -0300

    Refatoração: ListAthleteResponsibleController (Adicionado o endereço na listagem)

--- src/modules/athletes/controllers/ListAthleteResponsibleController.ts
+++ src/modules/athletes/controllers/ListAthleteResponsibleController.ts
@@ -1,16 +1,34 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { ListAthleteResponsibleUseCase } from "../useCases/listAthletesResponsible/ListAthleteResponsibleUseCase";
+import { ListAddressesUseCase } from "../../address/useCases/listAddresses/ListAddressesUseCase";
 
 class ListAthleteResponsibleController {
 
     async handle(request: Request, response: Response): Promise<Response> {
 
         const listAthleteResponsibleUseCase = container.resolve(ListAthleteResponsibleUseCase);
+        const listAdressesUseCase = container.resolve(ListAddressesUseCase);
 
         const allAthletesResponsible = await listAthleteResponsibleUseCase.execute();
+        const allAddresses = await listAdressesUseCase.execute();
+        
+        const dataAthletesResponsible = [];
+        for (let i = 0; i < allAthletesResponsible.length; i++) {
 
-        return response.status(200).json(allAthletesResponsible);
+            for (let j = 0; j < allAddresses.length; j++) {
+
+                if (allAthletesResponsible[i].addressId === allAddresses[j].id) {
+
+                    dataAthletesResponsible.push({
+                        AthleteResponsible: allAthletesResponsible[i],
+                        Address: allAddresses[j],
+                    });
+                }
+            }
+        }
+
+        return response.status(200).json(dataAthletesResponsible);
     }
 }
 
